735e0cc1e7b4f72f538720e0893271fb116deb39
VenomVendor <info@VenomVendor.com>
2017-02-25 18:54:51 +0530

    Initial commit for SDK

---
+++ src/androidTest/java/com/venomvendor/sdk/wordpress/ExampleInstrumentedTest.java
@@ -0,0 +1,33 @@
+/*
+ * Author		:	VenomVendor
+ * Dated		:	25-Feb'17 06:13:50 PM
+ * URL			:	https://www.google.co.in/search?q=VenomVendor
+ * Copyright(c):	2017 - Present, VenomVendor.
+ * License		:	Apache License Version 2.0
+ */
+package com.venomvendor.sdk.wordpress;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.venomvendor.sdk.wordpress.test", appContext.getPackageName());
+    }
+}
